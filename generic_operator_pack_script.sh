66a036daf8d1cd1c4c057f26324a8b0882b7ed6c
DavidDaskalo <david.daskalo@gmail.com>
2022-01-18 14:56:36 +0000

    Added parameter support and generification

---
+++ generic_operator_pack_script.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+print_help_message(){
+    echo "Usage:
+ bash-with-flags.sh -v [Openshift Version] -o [Operator Version] -r [Registry FQDN] -u [RedHat Account User] -p [Password] -n [Package Name]"
+}
+check_missing_params(){
+  [ -z $version ] && echo "Missing Openshift version" && print_help_message && exit 1
+  [ -z $operator ] && echo "Missing operator version" && print_help_message && exit 1
+  [ -z $registry ] && echo "Missing registry fqdn" && print_help_message && exit 1
+  [ -z $user ] && echo "Missing RedHat username" && print_help_message && exit 1
+  [ -z $password ] && echo "Missing RedHat user password" && print_help_message && exit 1
+  [ -z $name ] && echo  "Missing package name" && print_help_message && exit 1
+}
+
+while getopts ":v:o:r:u:p:n:" flag; do
+  case $flag in
+    v)version=$OPTARG >&2;;
+    o)operator=$OPTARG >&2;;
+    r)registry=$OPTARG >&2;;
+    u)user=$OPTARG >&2;;
+    p)password=$OPTARG >&2;;
+    n)name=$OPTARG >&2;;
+    ?)print_help_message >&2;;
+    *)echo "No argument was given!" >&2 ;;
+  esac
+done
+
+check_missing_params
+registry_base="/opt/$name-$version"
+. ./registry-creation-script.sh
+
+. /home/cloudlet/operator-packing-improvements/operator-packing-script.sh
--- registry_creation_script.sh
+++ registry_creation_script.sh
@@ -1,22 +1,21 @@
-## This is an automated procedure for creating an empty registry to be used for an operator whitening.
-##create all the necessary directories.
-mkdir /opt/registry
-echo 'Enter the registry FQDN you want to create: '
-read REGISTRY_FQDN
-export REGISTRY_BASE="/opt/registry"
-mkdir -p ${REGISTRY_BASE}/{auth,certs,data,downloads}
-mkdir -p ${REGISTRY_BASE}/downloads/{images,tools,secrets}
-echo "Created a bunch of directories"
+#!/bin/bash
+#mkdir /opt/acm-2.3.2-registry
+#echo 'Enter the registry FQDN you want to create: '
+#read registry
+
+#registry_base="/opt/$name-$version"
+
+mkdir -p ${registry_base}/{auth,certs,data,downloads}
+mkdir -p ${registry_base}/downloads/{images,tools,secrets}
 ## Add the registry FQDN to /etc/hosts
-echo "127.0.0.1      ${REGISTRY_FQDN}" >> /etc/hosts
-echo "Added the registry FQDN to /etc/hosts"
+echo "127.0.0.1      ${registry}" >> /etc/hosts
 ## Install necessary dependencies.
 yum install -y jq openssl podman  curl wget skopeo
 yum install -y  nmap telnet openldap-clients tcpdump
 yum install -y net-tools httpd-tools podman sqlite
-echo "Installed necessary packages"
-## Create a self-signed certificate for the registry
-cd ${REGISTRY_BASE}/certs/
+## Create a self-singed certificate for the registry
+cd ${registry_base}/certs/
+
 echo " [req]
 default_bits = 4096
 prompt = no
@@ -24,7 +23,6 @@ default_md = sha256
 x509_extensions = req_ext
 req_extensions = req_ext
 distinguished_name = dn
-
 [ dn ]
 C=US
 ST=New York
@@ -32,45 +30,43 @@ L=New York
 O=MyOrg
 OU=MyOU
 emailAddress=me@working.me
-CN=${REGISTRY_FQDN}
-
+CN=${registry}
 [ req_ext ]
 subjectAltName = @alt_names
 subjectKeyIdentifier = hash
 authorityKeyIdentifier = keyid:always,issuer
 basicConstraints = critical, CA:true
 keyUsage = critical, digitalSignature, cRLSign, keyCertSign
-
 [ alt_names ]
-DNS.1 = ${REGISTRY_FQDN}" > ${REGISTRY_BASE}/certs/answerFile.txt
-openssl req -newkey rsa:4096 -nodes -sha256 -keyout domain.key -x509 -days 365 -out domain.crt -config <( cat ${REGISTRY_BASE}/certs/answerFile.txt )
-echo "Created a domain.crt for the registry to have a certificate."
-cp ${REGISTRY_BASE}/certs/domain.crt /etc/pki/ca-trust/source/anchors/
+DNS.1 = ${registry}" > ${registry_base}/certs/answerFile.txt 
+openssl req -newkey rsa:4096 -nodes -sha256 -keyout domain.key -x509 -days 365 -out domain.crt -config <( cat ${registry_base}/certs/answerFile.txt )
+cp ${registry_base}/certs/domain.crt /etc/pki/ca-trust/source/anchors/
 update-ca-trust extract
 ## Create username and password to authenticate with the registry
-htpasswd -bBc ${REGISTRY_BASE}/auth/htpasswd admin redhat
-echo "Created username and password to authenticate with the registry"
+htpasswd -bBc ${registry_base}/auth/htpasswd admin redhat 
 ## Open the used port to allow pulling from the registry
 systemctl start firewalld
 firewall-cmd --add-port=5000/tcp --zone=public --permanent
 firewall-cmd --reload
-echo "Enabled traffic on port 5000 to be used by the registry."
 ## Create the registry
 echo 'podman run --name registry --rm -d -p 5000:5000 \
-        -v ${REGISTRY_BASE}/data:/var/lib/registry:z \
-        -v ${REGISTRY_BASE}/auth:/auth:z -e "REGISTRY_AUTH=htpasswd" \
-        -e "REGISTRY_AUTH_HTPASSWD_REALM=Registry" \
-        -e "REGISTRY_HTTP_SECRET=ALongRandomSecretForRegistry" \
-        -e REGISTRY_AUTH_HTPASSWD_PATH=/auth/htpasswd \
-        -v ${REGISTRY_BASE}/certs:/certs:z \
-        -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/domain.crt \
-        -e REGISTRY_HTTP_TLS_KEY=/certs/domain.key \
-docker.io/library/registry:2' > ${REGISTRY_BASE}/downloads/tools/start_registry.sh
-chmod a+x ${REGISTRY_BASE}/downloads/tools/start_registry.sh
-${REGISTRY_BASE}/downloads/tools/start_registry.sh
+	-v ${registry_base}/data:/var/lib/registry:z \
+	-v ${registry_base}/auth:/auth:z -e "REGISTRY_AUTH=htpasswd" \
+	-e "REGISTRY_AUTH_HTPASSWD_REALM=Registry" \
+	-e "REGISTRY_HTTP_SECRET=ALongRandomSecretForRegistry" \
+	-e REGISTRY_AUTH_HTPASSWD_PATH=/auth/htpasswd \
+	-v ${registry_base}/certs:/certs:z \
+	-e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/domain.crt \
+	-e REGISTRY_HTTP_TLS_KEY=/certs/domain.key \
+docker.io/library/registry:2' > ${registry_base}/downloads/tools/start_registry.sh
+chmod a+x ${registry_base}/downloads/tools/start_registry.sh
+source ${registry_base}/downloads/tools/start_registry.sh
+#${registry_base}/downloads/tools/start_registry.sh
 sleep 5
 echo ''
-curl -u admin:redhat -k https://$REGISTRY_FQDN:5000/v2/_catalog
+curl -u admin:redhat -k https://$registry:5000/v2/_catalog
 echo ''
 echo ''
-echo 'The script needs to return {"repositories":[]}, if everything worked.'
+echo 'The script needs to return {"repositories":[]}, if it did everything worked.'
+
+#source /home/cloudlet/operator-packing-improvements/operator-packing-script.sh
